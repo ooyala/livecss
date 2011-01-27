b9711e1ebed75088b3d8c86cfcd55f0f0198b383
T Beseda <taylor@quickleft.com>
2011-01-27 16:07:20 -0700

    no need for fn to check search portion of location, just indexOf for livecss=true

--- livecsshelper.js
+++ livecsshelper.js
@@ -4,20 +4,7 @@
  * live css will watch on every reload as long as the querystring is there and true
  */
 
-function getParameterByName( name )
-{
-  // http://stackoverflow.com/questions/901115/get-querystring-values-with-jquery
-  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-  var regexS = "[\\?&]"+name+"=([^&#]*)";
-  var regex = new RegExp( regexS );
-  var results = regex.exec( window.location.href );
-  if( results == null )
-    return "";
-  else
-    return decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
 $(document).ready(function(){
-    if( getParameterByName( 'livecss' ) === "true" )
+    if( window.location.search.indexOf('livecss=true') > 0 )
         livecss.watchAll();
 });
